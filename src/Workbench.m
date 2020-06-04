7f206e2e6623ef76764afd8ffc162a88d1723abe
Richard-Schadeberg <64522241+Richard-Schadeberg@users.noreply.github.com>
2020-06-04 14:59:06 +1000

    started rendering toaster

--- src/Workbench.m
+++ src/Workbench.m
@@ -6,8 +6,8 @@ breadTr = transl(0.1, 0.1, 0);
 bread = Bread(breadTr);
 bread2EffTr = transl(0, 0, 0.05) * trotx(pi);
 
-toasterTr = transl(0.08,0,0);
-toaster2Slot1Tr = toasterTr*transl(0.01,0,0.02);
+toasterTr = transl(0.15,0,0);
+toaster2Slot1Tr = transl(0,0.0126,0.006103);
 
 testAnimate(robot, q, bread, breadTr, bread2EffTr, toasterTr, toaster2Slot1Tr);
 
@@ -21,6 +21,10 @@ function testAnimate(robot, startQ, bread, breadTr, bread2EffTr, toasterTr, toas
     numSteps = 120;
     isHolding = false;
     
+    hold on
+    toaster_h=createProp("toaster.ply",toasterTr,[1 1 1])
+    hold off
+    
     %go to first location above toast
     goalTr = breadTr * transl(0,0,0.1) * bread2EffTr; % location 100mm above bread grabbing point
 	q = moveRobotJoints(robot, goalTr, isHolding, bread, eff2BreadTr, q, numSteps);
@@ -33,13 +37,13 @@ function testAnimate(robot, startQ, bread, breadTr, bread2EffTr, toasterTr, toas
 	isHolding = true;
 
     %lift bread up 100mm
-	goalTr = breadTr * transl(0,0,0.1) * bread2EffTr;
-	q = moveRobotCartesian(robot, goalTr, isHolding, bread, eff2BreadTr, q);
+	goalTr = breadTr * transl(0,0,0.08) * bread2EffTr;
+	q = moveRobotJoints(robot, goalTr, isHolding, bread, eff2BreadTr, q, numSteps);
     
 	%lift bread to above the toaster
     % when end effector is at the following location, bread will be 15cm
     % above slot 1
-    goalTr = toasterTr * toaster2Slot1Tr * transl(0,0,0.15) * bread2EffTr ;
+    goalTr = toasterTr * toaster2Slot1Tr * transl(0,0,0.2) * bread2EffTr ;
 	q = moveRobotJoints(robot, goalTr, isHolding, bread, eff2BreadTr, q, numSteps);
     
     %slide the bread into the toaster, slot 1
@@ -47,6 +51,17 @@ function testAnimate(robot, startQ, bread, breadTr, bread2EffTr, toasterTr, toas
 	q = moveRobotCartesian(robot, goalTr, isHolding, bread, eff2BreadTr, q);
 end
 
+function prop_h = createProp(propName, locationTr, colour)
+    [faces, points, data] = plyread(propName, "tri");
+    hold on
+    prop_h=trisurf(faces,points(:,1),points(:,2),points(:,3),"LineStyle","none","Facecolor",colour);
+    hold off
+    numPoints = size(points);
+    self.numPoints = numPoints(1);
+    for j=1:self.numPoints
+        prop_h.Vertices(j,:)=transl(locationTr*transl(points(j,:)))';
+    end
+end
 
 function endJoints = moveRobotCartesian(robot, goalTr, isHolding, prop, eff2PropTr, startJoints)
     qMatrix = robot.getCartesianQMatrix(startJoints, goalTr);
